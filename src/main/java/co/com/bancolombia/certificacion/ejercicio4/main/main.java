2050bd9f2ab0957c33508a2f98f74f68ae5a789b
dcastril <dcastril@bancolombia.com.co>
2018-11-21 09:21:25 -0500

    corregir error en el main del ejercicio4

--- src/main/java/co/com/bancolombia/certificacion/ejercicio4/main/main.java
+++ src/main/java/co/com/bancolombia/certificacion/ejercicio4/main/main.java
@@ -3,17 +3,12 @@ package co.com.bancolombia.certificacion.ejercicio4.main;
 import co.com.bancolombia.certificacion.ejercicio4.model.ColeccionInterfaz;
 import co.com.bancolombia.certificacion.ejercicio4.model.Lista;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class main {
 
-    public static void main (String[]){
+    public static void main (String[] args){
         ColeccionInterfaz<String> milista = new Lista();
         milista.añadir("asa");
 
-        List<String> otraLista = new ArrayList<>();
-        otraLista.add("");
     }
 
 
